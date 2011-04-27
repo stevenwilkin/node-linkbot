e4270ce35a7bc8308ad2d086d475b7979759e5a6
Steven Wilkin <stevenwilkin@gmail.com>
2011-04-28 00:17:04 +0100

    organise code into an object literal

--- linkbot.js
+++ linkbot.js
@@ -7,34 +7,59 @@ const
 	CHANNEL = '#biscuitbot',
 	NICK = 'biscuitbot';
 
-console.log('> starting bot');
-
 var start = new Date();
 
-var client = new irc.Client(SERVER, NICK, {
-	channels: [CHANNEL]
-});
 
-client.on('join', function() {
-	console.log('> joined room');
-});
+bot = {
+
+	client: null,
 
 
-client.on('message' + CHANNEL, function(from, message) {
-	var matches = message.match(/^!(\w+)/)
-	if(matches) {
-		var command = matches[1];
-		console.log('> command: %s', command);
-		if(command.match(/uptime/i)) {
-			var uptime = new Date - start;
-			client.say(CHANNEL, uptime + 's');
+	/**
+	 * Initialise the irc bot
+	 */
+	init: function() {
+
+		console.log('> starting bot');
+
+		var client = bot.client = new irc.Client(SERVER, NICK, {
+			channels: [CHANNEL]
+		});
+		
+		client.on('join', function() {
+			console.log('> joined room');
+		});
+		
+		
+		client.on('message' + CHANNEL, bot.message);
+	},
+
+
+	/**
+	 * Process messages from irc channel
+	 *
+	 * @param from    string Nick originating the message
+	 * @param message string Content of the message
+	 */
+	message: function(from, message) {
+		var matches = message.match(/^!(\w+)/)
+		if(matches) {
+			var command = matches[1];
+			console.log('> command: %s', command);
+			if(command.match(/uptime/i)) {
+				var uptime = new Date - start;
+				bot.client.say(CHANNEL, uptime + 's');
+			}
+			return;
 		}
-		return;
+	
+		var matches = message.match(/(https?\:\/\/[^ ]+)/);
+		if(matches) {
+			console.log('> link mentioned');
+			bot.client.say(CHANNEL, matches[1]);
+		}
 	}
+}
 
-	var matches = message.match(/(https?\:\/\/[^ ]+)/);
-	if(matches) {
-		console.log('> link mentioned');
-		client.say(CHANNEL, matches[1]);
-	}
-});
+
+bot.init();
